f7b5f539e7fb215a53585d1e2083e72c87df526e
LaulinVasquez <laurin171125@gmail.com>
2023-04-20 20:16:06 -0400

    using conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade porcentage: ");
+        int porcentage = Convert.ToInt32(Console.ReadLine());
+        
+        string letter = "";
+
+        if (porcentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (porcentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (porcentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (porcentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (porcentage < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (porcentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry but you can try it next time!");
+        }
     }
 }
