0997324f51f3c06773f2c77ca36e3eb4ec2b2ab9
LaulinVasquez <laurin171125@gmail.com>
2023-04-24 21:43:29 -0400

    prep 3 and prep 4 done, check comments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Here I declared the random function between 1 - 100
+        Random rnd = new Random();
+        int magicNumber = rnd.Next(1,100);
+        
+        // I declared my guess variable so that I can make the question in the 
+        // while loop
+        int guessNumber = 0;
+        int counter = 0;
+        string user = "";
+        while (user != "no")
+        {
+            // you can increase an int variable using "++" that will add one value each
+            // time the while loop repeats
+            counter++;
+            Console.Write("What is your guess? ");
+            guessNumber = Convert.ToInt32(Console.ReadLine());
+            
+            if (guessNumber < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guessNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine($"you guess it, that took you {counter} times");
+            Console.Write("Do you want to play again? ");
+            user = Console.ReadLine();
+        }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of number, type 0 when finished");
+        List<int> numbers = new List<int>();
+
+        int user = -1;
+        while (user != 0)
+        {
+            Console.Write("Enter a number: ");
+            user = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(user);
+            // This if statement is used to remove the "0"  stored from our list
+            if (user == 0)
+            {
+                numbers.Remove(0);
+            }
+        }
+        // sum is the method to get the total of my list
+        int total = numbers.Sum();
+        Console.WriteLine($"the sum is: {total}");
+        // Compute the average of the numbers in the list.
+        double averageList = numbers.Average();
+        Console.WriteLine($"Average: {averageList}");
+        // Get the largest number if the list using "max()" method
+        int largestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
+        // iterate in the list using a foreach
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
