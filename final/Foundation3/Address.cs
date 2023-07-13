d6d8020df5fb43bd34d987d085e920dc7b0f2eee
LaulinVasquez <laurin171125@gmail.com>
2023-07-12 20:35:43 -0400

    event marketing template created

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _zipcode;
+
+    public Address(string street, string city, string state, string zipcode)
+    {
+        _street = street;
+        _city = city;
+        _state = state;
+        _zipcode = zipcode;
+    }
+    // Create getters for the street and city and full  address
+    public string street
+    {
+        get { return _street;}
+    }
+    public string city
+    {
+        get { return _city;}
+    }
+    public string FullAddress
+    {
+        get { return $"{_street}, {_city}, {_state } {_zipcode}";}
+    }
+
+}
---
+++ final/Foundation3/OutdoorGatherings.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class OutdoorGatherings : Event
+{
+    private string _weatherForecast;
+
+    public OutdoorGatherings(string weather, Address address, string title, string description, string eventType, string date, string time, string eventInformation) : base( address, title, description, eventType, date, time, eventInformation)
+    {
+        _weatherForecast = weather;
+    }
+    public void GetFullDetails()
+    {
+        FullDetails();
+        Console.WriteLine($"Weather statement: {_weatherForecast}");
+    }
+    public void DisplayMarketingTemplate()
+    {
+        Console.WriteLine("Short description:\n");
+        Console.WriteLine(StandarDetails);
+        Console.WriteLine("\nFull details presentation:\n");
+        GetFullDetails();
+        Console.WriteLine("\nShort description:\n");
+        Console.WriteLine(ShortDescription);
+    }
+}
